301706b7ec4c1f3abbf6b6553c72733820c7ac6a
John Chittam <john@jchittam.com>
2020-11-25 14:33:09 -0500

    initial commit

---
+++ Main.asm
@@ -0,0 +1,93 @@
+; 10 SYS (2304)
+
+*=$0801
+
+        BYTE    $0E, $08, $0A, $00, $9E, $20, $28,  $32, $33, $30, $34, $29, $00, $00, $00
+
+
+*=$0900
+PROGRAM_START
+
+        
+
+PROGRAM_END
+        rts
+        
+
+
+
+EYE_FRONT_DATA
+; eye_front
+ BYTE $00,$7E,$00
+ BYTE $03,$81,$C0
+ BYTE $0C,$7E,$30
+ BYTE $13,$FF,$C8
+ BYTE $2F,$FF,$F4
+ BYTE $2F,$FF,$F4
+ BYTE $5F,$FF,$FA
+ BYTE $5F,$C3,$FA
+ BYTE $5F,$81,$FA
+ BYTE $BF,$00,$FD
+ BYTE $BF,$00,$FD
+ BYTE $BF,$00,$FD
+ BYTE $5F,$81,$FA
+ BYTE $5F,$C3,$FA
+ BYTE $5F,$FF,$FA
+ BYTE $2F,$FF,$F4
+ BYTE $2F,$FF,$F4
+ BYTE $13,$FF,$C8
+ BYTE $0C,$7E,$30
+ BYTE $03,$81,$C0
+ BYTE $00,$7E,$00
+ BYTE $00
+
+EYE_LEFT_DATA
+; eye_left
+ BYTE $00,$7E,$00
+ BYTE $03,$81,$C0
+ BYTE $0C,$7E,$30
+ BYTE $13,$FF,$C8
+ BYTE $2F,$FF,$F4
+ BYTE $2F,$FF,$F4
+ BYTE $5F,$FF,$FA
+ BYTE $5C,$7F,$FA
+ BYTE $58,$3F,$FA
+ BYTE $B8,$3F,$FD
+ BYTE $B8,$3F,$FD
+ BYTE $B8,$3F,$FD
+ BYTE $58,$3F,$FA
+ BYTE $5C,$7F,$FA
+ BYTE $5F,$FF,$FA
+ BYTE $2F,$FF,$F4
+ BYTE $2F,$FF,$F4
+ BYTE $13,$FF,$C8
+ BYTE $0C,$7E,$30
+ BYTE $03,$81,$C0
+ BYTE $00,$7E,$00
+ BYTE $00
+
+EYE_RIGHT_DATA
+; eye right
+ BYTE $00,$7E,$00
+ BYTE $03,$81,$C0
+ BYTE $0C,$7E,$30
+ BYTE $13,$FF,$C8
+ BYTE $2F,$FF,$F4
+ BYTE $2F,$FF,$F4
+ BYTE $5F,$FF,$FA
+ BYTE $5F,$FE,$3A
+ BYTE $5F,$FC,$1A
+ BYTE $BF,$FC,$1D
+ BYTE $BF,$FC,$1D
+ BYTE $BF,$FC,$1D
+ BYTE $5F,$FC,$1A
+ BYTE $5F,$FE,$3A
+ BYTE $5F,$FF,$FA
+ BYTE $2F,$FF,$F4
+ BYTE $2F,$FF,$F4
+ BYTE $13,$FF,$C8
+ BYTE $0C,$7E,$30
+ BYTE $03,$81,$C0
+ BYTE $00,$7E,$00
+ BYTE $00
+
